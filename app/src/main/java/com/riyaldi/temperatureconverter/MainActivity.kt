2a6165711fb2579fffbe251bddbf159538f216ac
Riyaldi Hasan <riy.2923@gmail.com>
2020-09-10 20:42:43 +0700

    add converter function and form validation

--- app/src/main/java/com/riyaldi/temperatureconverter/MainActivity.kt
+++ app/src/main/java/com/riyaldi/temperatureconverter/MainActivity.kt
@@ -9,7 +9,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     companion object {
-        const val c = "Celcius"
+        const val c = "Celsius"
         const val f = "Fahrenheit"
         const val k = "Kelvin"
         const val r = "Reaumur"
@@ -30,7 +30,12 @@ class MainActivity : AppCompatActivity() {
         btTemp1.text = temp1
         btTemp2.text = temp2
         button.setOnClickListener {
-            countTemp()
+            if (etTemp1.text!!.isNotEmpty())
+                countTemp()
+            else {
+                etTemp1.error = "Kosong"
+                Toast.makeText(this, "Form harus di isi", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
@@ -65,53 +70,101 @@ class MainActivity : AppCompatActivity() {
         when {
             temp1 == c && temp2 == f -> {
                 val value = ctof(etTemp1.text.toString().toDouble()).toString()
-                tvTempValue.setText(value)
+                tvTempValue.text = value
             }
             temp1 == c && temp2 == r -> {
                 val value = ctor(etTemp1.text.toString().toDouble()).toString()
-                tvTempValue.setText(value)
+                tvTempValue.text = value
             }
             temp1 == c && temp2 == k -> {
-//                ctok()
+                val value = ctok(etTemp1.text.toString().toDouble()).toString()
+                tvTempValue.text = value
             }
             temp1 == f && temp2 == c -> {
-//                ftoc()
+                val value = ftoc(etTemp1.text.toString().toDouble()).toString()
+                tvTempValue.text = value
             }
             temp1 == f && temp2 == r -> {
-//                ftor()
+                val value = ftor(etTemp1.text.toString().toDouble()).toString()
+                tvTempValue.text = value
             }
             temp1 == f && temp2 == k -> {
-//                ftok()
+                val value = ftok(etTemp1.text.toString().toDouble()).toString()
+                tvTempValue.text = value
             }
             temp1 == r && temp2 == c -> {
-//                rtoc()
+                val value = rtoc(etTemp1.text.toString().toDouble()).toString()
+                tvTempValue.text = value
             }
             temp1 == r && temp2 == f -> {
-//                rtof()
+                val value = rtof(etTemp1.text.toString().toDouble()).toString()
+                tvTempValue.text = value
             }
             temp1 == r && temp2 == k -> {
-//                rtok()
+                val value = rtok(etTemp1.text.toString().toDouble()).toString()
+                tvTempValue.text = value
             }
             temp1 == k && temp2 == c -> {
-//                ktoc()
+                val value = ktoc(etTemp1.text.toString().toDouble()).toString()
+                tvTempValue.text = value
             }
             temp1 == k && temp2 == f -> {
-//                ktof()
+                val value = ktof(etTemp1.text.toString().toDouble()).toString()
+                tvTempValue.text = value
             }
             temp1 == k && temp2 == r -> {
-//                ktor()
+                val value = ktor(etTemp1.text.toString().toDouble()).toString()
+                tvTempValue.text = value
             }
             else -> Toast.makeText(this, "Tidak dapat mengkonversi 2 temperatur yang sama", Toast.LENGTH_LONG).show()
         }
     }
 
     private fun ctof(c: Double): Double {
-        val hasil = c * (9.0 / 5.0) + 32.0
-        return hasil
+        return c * (9.0 / 5.0) + 32.0
     }
 
     private fun ctor(c: Double): Double {
-        val hasil = c * (4.0 / 5.0)
-        return hasil
+        return c * (4.0 / 5.0)
+    }
+
+    private fun ctok(c: Double): Double {
+        return c + 273.15
+    }
+
+    private fun ftoc(f: Double): Double {
+        return (f - 32.0) * (5.0 / 9.0)
+    }
+
+    private fun ftor(f: Double): Double {
+        return (f - 32.0) * (4.0 / 9.0)
+    }
+
+    private fun ftok(f: Double): Double {
+        return ((f - 32.0) * (5.0 / 9.0)) - 273.15
+    }
+
+    private fun rtoc(r: Double): Double {
+        return r * (5.0 / 4.0)
+    }
+
+    private fun rtof(r: Double): Double {
+        return r * (9.0 / 4.0) + 32.0
+    }
+
+    private fun rtok(r: Double): Double {
+        return r * (5.0 / 4.0) + 273.15
+    }
+
+    private fun ktoc(k: Double): Double {
+        return k - 273.15
+    }
+
+    private fun ktof(k: Double): Double {
+        return (k - 273.15) * (9.0 / 5.0) + 32.0
+    }
+
+    private fun ktor(k: Double): Double {
+        return (k - 273.15) * (4.0 / 5.0)
     }
 }
